7b36780589b314983f17523820aef4b939d2651b
Matthew0898 <Matthew0898@Matthew0898.com>
2017-12-06 13:55:22 -0600

    file renaming. Also closed the scanner.

--- src/main/ImageManager.java
+++ src/main/ImageManager.java
@@ -1,15 +1,18 @@
 package main;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import java.text.SimpleDateFormat;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
 public class ImageManager {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Enter the path of a file or directory");
 		String path = in.next();
+		in.close();
 		File f = new File(path);
 		if (f.exists()) {
 			if (f.isFile()) {
@@ -30,10 +33,19 @@ public class ImageManager {
 		return sdf.format(in.lastModified());
 	}
 	
-	
+	/**
+	 * @see https://docs.oracle.com/javase/7/docs/api/java/nio/file/Files.html#move(java.nio.file.Path,%20java.nio.file.Path,%20java.nio.file.CopyOption...)
+	 * @see https://stackoverflow.com/questions/1158777/rename-a-file-using-java
+	 */
 	public static void rename(File in) {
 		String newName = generateName(in);
-		//Rename file
+		Path originalPath = in.toPath();
+		try {
+		Files.move(originalPath, originalPath.resolveSibling(newName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
 		
 	}
 	

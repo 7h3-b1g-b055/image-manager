70755470f4392ab6ee7bdab96af4592cfc2f6d49
paulk <paulk@Covfefe-the-wise>
2017-12-12 22:41:35 -0600

    works on windows

--- src/main/ImageManager.java
+++ src/main/ImageManager.java
@@ -8,9 +8,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 public class ImageManager {
 
+	/**
+	 * Main method for running the program.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter the path of a file or directory");
+		System.out.println("Enter the path of a file or directory");
 		String path = in.next();
 		in.close();
 		File f = new File(path);
@@ -28,14 +32,21 @@ public class ImageManager {
 		}
 	}
 	
+	/**
+	 * Generates a name for a given file.
+	 * @param in file to base name on
+	 * @return the generated name as a string
+	 */
 	public static String generateName(File in) {
-		SimpleDateFormat sdf = new SimpleDateFormat("MM.dd.yy HH:mm:ss");
+		SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yy HH~mm~ss");
 		return sdf.format(in.lastModified());
 	}
 	
 	/**
+	 * Renames a file.
 	 * @see https://docs.oracle.com/javase/7/docs/api/java/nio/file/Files.html#move(java.nio.file.Path,%20java.nio.file.Path,%20java.nio.file.CopyOption...)
 	 * @see https://stackoverflow.com/questions/1158777/rename-a-file-using-java
+	 * @param in the file to rename
 	 */
 	public static void rename(File in) {
 		String newName = generateName(in);
@@ -45,10 +56,11 @@ public class ImageManager {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-		
 	}
-	
+	/**
+	 * Calls rename on a folder of files.
+ * @param in the folder to rename
+	 */
 	public static void massEdit(File in) {
 		File[] folder = in.listFiles();
 		for (File file : folder) {
